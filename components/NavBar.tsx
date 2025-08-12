375b25954ca26bf767422254a62697a7dc0171a1
Animesh <2022ugee018@nitjsr.ac.in>
2025-08-12 19:55:50 +0530

    Add Clerk Authentication

---
+++ components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { checkUser } from '@/lib/checkUser'
+
+export default function NavBar() {
+  const user= checkUser()
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
